29eba654d7694e046db39297cdbec88bbc0872be
Artem Pivko <69391262+1MarBur1@users.noreply.github.com>
2023-01-21 00:33:48 +0300

    init storybook, need to fix styling

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import '../styles/fonts.css'
+import '../styles/globals.css'
+import '../styles/typeface.css'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
